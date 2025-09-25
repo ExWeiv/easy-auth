21376a0ae662582bf536d3d60fbf15a158666c18
Enes <loeiks@icloud.com>
2025-09-26 02:21:40 +0300

    Last update before deprecating package.

--- app/lib/Errors/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.provider = exports.prefix = void 0;
-exports.prefix = 'Easy Auth Error:';
-exports.provider = ["Facebook", "Google", "GitHub", "Discord", "Steam"];
-exports.default = {
-    prefix: exports.prefix,
-    provider: exports.provider
-};
